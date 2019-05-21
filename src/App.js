819fb3a5f5c4d18fdc6893fa1fbab773c168e878
Anton Serdyuchenko <anton415@gmail.com>
2019-05-21 06:20:31 +0300

    Add test video.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Video from './utils/video/video.mp4'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello World!
+        <video ref={this.setVideoRef} controls >
+          <source src={Video} type="video/mp4" />
+        </video>
       </div>
     );
   }
